d13a36679138596353498a09e172467e4dd17307
Laurits <laurits.mumberg@gmail.com>
2020-10-09 21:44:27 +0200

    Naxiel news added

--- MenuProgram/MenuProgram/Program.cs
+++ MenuProgram/MenuProgram/Program.cs
@@ -20,6 +20,7 @@ namespace MenuProgram
             // TO DO Naxiel twitter
             menu.Add(new RSSMenu("Dota 2 News", "https://blog.dota2.com/feed/"));
             menu.Add(new RSSMenu("DR Nyheder", "https://www.dr.dk/nyheder/service/feeds/allenyheder"));
+            menu.Add(new RSSMenu("Naxiel News", "https://rss.app/feeds/aNChsspxb2mudCK4.xml"));
             menu.Start();
         }
     }
